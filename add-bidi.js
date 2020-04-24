9ccaf4e7bb550ff5e254967660a04a83ed08e26e
Mostafa Ahangarha <ahangarha@gmail.com>
2020-04-25 03:30:43 +0430

    monitor DOM updates

--- add-bidi.js
+++ add-bidi.js
@@ -1,10 +1,28 @@
-const all_elements = document.querySelectorAll("h1,h2,h3,h4,h5,h6,p,a");
+const add_bidi_support = function() {
+  const all_elements = document.querySelectorAll("h1,h2,h3,h4,h5,h6,p,a");
 
-all_elements.forEach(element => {
-  element.setAttribute("dir","auto");
+  all_elements.forEach(element => {
+    element.setAttribute("dir","auto");
 
-  element_style = window.getComputedStyle(element)['text-align'];
-  if (element_style === "left"){
-    element.setAttribute("style", "text-align: start");
-  }  
-});
+    element_style = window.getComputedStyle(element)['text-align'];
+    if (element_style === "left"){
+      element.setAttribute("style", "text-align: start");
+    }  
+  });
+}
+
+// Run the main function once
+console.log("initial add_bidi_support run...");
+add_bidi_support();
+
+// Monitor for adding new element in DOM
+let targetNode = document.querySelector('body');
+const config = { childList: true, subtree: true };
+
+const callback = function(mutationsList, observer) {
+  console.log("Running add_bidi_support because of DOM update");
+  add_bidi_support();
+};
+
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
