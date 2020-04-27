b7503afa381caab30dbdc2a3aa897d113a0d95b0
Mostafa Ahangarha <ahangarha@gmail.com>
2020-04-27 17:25:42 +0430

    reduce update gap time

--- add-bidi.js
+++ add-bidi.js
@@ -18,9 +18,33 @@ const add_bidi_support = () => {
   observer.observe(targetNode, config);
 }
 
+<<<<<<< HEAD
 const callback = () => {
   console.log("Run add_bidi_support because of DOM update");
   add_bidi_support();
+=======
+// Run the main function once
+console.log("initial add_bidi_support run...");
+let last_process_time = Date.now();
+add_bidi_support();
+
+// Monitor for adding new element in DOM
+let targetNode = document.querySelector('body');
+const config = { childList: true, subtree: true };
+
+const callback = function(mutationsList, observer) {
+  // preventing processing for too quick updates
+  const since_last_process = Date.now() - last_process_time;
+  const update_gap = 1000;
+  if (since_last_process > update_gap){
+    console.log("Run add_bidi_support because of DOM update");
+    add_bidi_support();
+    last_process_time = Date.now();
+  } else {
+    console.log(`Reject add bidi: fast updates in ${since_last_process} ms!`)    
+  }
+  
+>>>>>>> reduce update gap time
 };
 
 observer = new MutationObserver(callback);
