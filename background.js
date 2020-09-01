7c7adeeecd558e62b9f468a8147efb5e40c84044
Mostafa Ahangarha <ahangarha@riseup.net>
2020-09-01 18:06:39 +0430

    remove popup and make icon clickable

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
-browser.browserAction.setPopup({
-  popup: browser.runtime.getURL('./popup/index.html'),
-});
+async function statusHandle(enabled){
+  const iconPath = enabled ? "../assets/icon/bidi-48px.png" : "../assets/icon/bidi-48px-disabled.png";
+  browser.browserAction.setIcon({path: iconPath});
+  await browser.tabs.reload();
+}
+
+async function iconClicked() {
+  const { enabled } = await browser.storage.local.get({ enabled: true });
+  await browser.storage.local.set({ enabled: !enabled });
+  statusHandle(!enabled)
+}
+console.log("test");
+
+browser.browserAction.onClicked.addListener(iconClicked);
