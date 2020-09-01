c9562f4b826fe706da17cd7379f84b721dc2eeb6
Mostafa Ahangarha <ahangarha@riseup.net>
2020-09-01 17:39:50 +0430

    refactor and support icon change

--- popup/index.js
+++ popup/index.js
@@ -1,11 +1,19 @@
-const popupContent = document.getElementById('popupContent');
+async function statusHandle(enabled){
+  const popupContent = document.getElementById('popupContent');
+  const input = popupContent.getElementsByTagName("input")[0];
+  const label = popupContent.getElementsByTagName("label")[0];
+  input.checked = enabled ? true : false;
+  label.innerText = enabled ? 'Enabled' : 'Disabled';
+
+  const iconPath = enabled ? "../assets/icon/bidi-48px.png" : "../assets/icon/bidi-48px-disabled.png";
+  browser.browserAction.setIcon({path: iconPath});
+
+  await browser.tabs.reload();
+}
 
 const reload = async () => {
   const { enabled } = await browser.storage.local.get({ enabled: true });
-  input = popupContent.getElementsByTagName("input")[0];
-  label = popupContent.getElementsByTagName("label")[0];
-  input.checked = enabled ? true : false;
-  label.innerText = enabled ? 'Enabled' : 'Disabled';
+  statusHandle(enabled)
 };
 
 reload();
@@ -13,9 +21,5 @@ reload();
 popupContent.onclick = async () => {
   const { enabled } = await browser.storage.local.get({ enabled: true });
   await browser.storage.local.set({ enabled: !enabled });
-  input = popupContent.getElementsByTagName("input")[0];
-  label = popupContent.getElementsByTagName("label")[0];
-  input.checked = !enabled ? true : false;
-  label.innerText = !enabled ? 'Enabled' : 'Disabled';
-  await browser.tabs.reload();
+  statusHandle(!enabled)
 };
